a0a56d5dd2a8d6f06fa015d895eb7715f7e47166
Harris <harris@Harriss-Air.hitronhub.home>
2022-10-13 22:31:22 -0400

    Add Database collector to store infomration

--- scraper.py
+++ scraper.py
@@ -2,6 +2,14 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import sqlite3
+
+# Create connection to sqlite3
+conn = sqlite3.connect('results.db')
+c = conn.cursor()
+
+# Create a Database called results
+c.execute('''CREATE TABLE results(prod_name TEXT, price INT, link TEXT)''')
 
 # Determine what product the user is looking for
 product = input("What product would you like to search for? ").lower()
@@ -27,7 +35,7 @@ for page in range(1, pages + 1):
     search_page = soup.find(class_='item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell')
     search_items = search_page.find_all(text=re.compile(product)) 
 
-    # Obtain link and price for each product on the page
+    # Obtain link and price for each product on the page and store it into results
     for items in search_items:
         items_parent = items.parent
         if items_parent.name != 'a':
@@ -37,4 +45,23 @@ for page in range(1, pages + 1):
         price = price_tag.find(class_='price-current').strong.string
         link = items_parent['href']
 
-        print(link, price)
+        # Store information into database results
+        c.execute('''INSERT INTO results VALUES(?, ?, ?)''',(items, price, link))
+        conn.commit()
+
+# Select all the information from the database results
+# Order the information from the database in ascending order by price
+c.execute('''SELECT * FROM results ORDER BY price ASC''')
+output = c.fetchall()
+
+# Output all the products with their respective prices and links from the database
+for i in range(len(output)):
+    print('Product: ' + output[i][0])
+    print('Price: $' + str(output[i][1]))
+    print('Link: ' + output[i][2])
+    print("\n")
+
+# Drop the table results
+c.execute("DROP TABLE results")
+conn.commit()
+conn.close()
